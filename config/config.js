ac5af39a69bb7fb88adf41108de36269588e8111
sulthanallaudeen <sa@sulthanallaudeen.com>
2019-07-16 06:40:42 +0530

    Project Config & Env Library

---
+++ config/config.js
@@ -0,0 +1,9 @@
+var app = {
+    name: 'Sulthan Allaudeen',
+    local_url: 'localhost:3000',
+    live_url: 'sulthanallaudeen/api/v2'
+}
+
+var config = {};
+config.app = app;
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var env = require('dotenv').config();
+var config = require("../config/config");
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(config.app.name);
+  res.render('index', { title: env.parsed.app_name });
 });
 
 module.exports = router;
